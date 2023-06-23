608753f98e459dc362e1c2d60216cb6ffc3bab5e
ErikaCubillos <82921108+erikacubillos@users.noreply.github.com>
2023-06-23 12:46:45 -0500

    add files shopping and index

---
+++ js/carrito.js
@@ -0,0 +1,10 @@
+function comprarCarrito() {
+
+    productosEnCarrito.length = 0;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.remove("disabled");
+}
